c0a822c49781e3a425a91ec7ace12d45bc4716be
Eric Little <eric.j.little@gmail.com>
2016-05-18 08:58:47 -0400

    Commented JQuery code.

--- js/javascript.js
+++ js/javascript.js
@@ -28,14 +28,23 @@ $(document).ready(function() {
 	$('#postReview').on('click',function(e) {
 		e.preventDefault();
 
-		if ($('#reviewer').val() != '' && $('#comments').val() != '') {
+		//Validate form
+		if (
+			$('#reviewer').val() != '' 
+			&& $('#comments').val() != ''
+		) {
+			//Copy an existing review
 			var copy = $('.review:first').clone();
+
+			//Change the values to what was submitted
 			copy.find('.rating').text($('#rating option:selected').text());
 			copy.find('h3').text($('#reviewer').val());
 			copy.find('.comment').text($('#reviewform #comments').val());
 
+			//Add the review to the end of the list
 			$('#reviews').append(copy);
 
+			//Reset the form
 			$('#reviewer').val('');
 			$('#comments').val('');
 			$('#rating option').prop('selected',false);
